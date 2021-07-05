7ff823596c788f1d427bbe7b8b7d578538d3c642
Kirilll <vilyam13@gmail.com>
2021-07-05 12:36:54 +0300

    random quiz generator

---
+++ randomQuizGenerator/randomQuizGenerator.py
@@ -0,0 +1,53 @@
+import random
+
+# The quiz data. Keys are states and values are their capitals.
+capitals = {'Alabama': 'Montgomery', 'Alaska': 'Juneau', 'Arizona': 'Phoenix', 'Arkansas': 'Little Rock',
+            'California': 'Sacramento', 'Colorado': 'Denver', 'Connecticut': 'Hartford', 'Delaware': 'Dover',
+            'Florida': 'Tallahassee', 'Georgia': 'Atlanta', 'Hawaii': 'Honolulu', 'Idaho': 'Boise',
+            'Illinois': 'Springfield', 'Indiana': 'Indianapolis', 'Iowa': 'Des Moines', 'Kansas': 'Topeka',
+            'Kentucky': 'Frankfort', 'Louisiana': 'Baton Rouge', 'Maine': 'Augusta', 'Maryland': 'Annapolis',
+            'Massachusetts': 'Boston', 'Michigan': 'Lansing', 'Minnesota': 'Saint Paul', 'Mississippi': 'Jackson',
+            'Missouri': 'Jefferson City', 'Montana': 'Helena', 'Nebraska': 'Lincoln', 'Nevada': 'Carson City',
+            'New Hampshire': 'Concord', 'New Jersey': 'Trenton', 'New Mexico': 'Santa Fe', 'New York': 'Albany',
+            'North Carolina': 'Raleigh', 'North Dakota': 'Bismarck', 'Ohio': 'Columbus', 'Oklahoma': 'Oklahoma City',
+            'Oregon': 'Salem', 'Pennsylvania': 'Harrisburg', 'Rhode Island': 'Providence', 'South Carolina': 'Columbia',
+            'South Dakota': 'Pierre', 'Tennessee': 'Nashville', 'Texas': 'Austin', 'Utah': 'Salt Lake City',
+            'Vermont': 'Montpelier', 'Virginia': 'Richmond', 'Washington': 'Olympia', 'West Virginia': 'Charleston',
+            'Wisconsin': 'Madison', 'Wyoming': 'Cheyenne'}
+
+# Generate 35 quiz files.
+for quizNum in range(35):
+	# Создать файлы билетов и файлы ключей ответов
+	quizFile = open('.\exam_tickets\questions\capitalsquiz%s.txt' % (quizNum + 1), 'w')
+	answerKeyFile = open('.\exam_tickets\\answerKeys\capitalsquiz_answers%s.txt' % (quizNum + 1), 'w')
+
+	# Записать заголовок билета
+	quizFile.write('Имя:\n\nДата:\n\nКурс:\n\n')
+	quizFile.write((' ' * 15) + 'Проверка знания столиц штатов (Билет %s)' % (quizNum + 1))
+	quizFile.write('\n\n')
+
+	# Перемешать порядок следования штатов
+	states = list(capitals.keys())
+	random.shuffle(states)
+
+	# Организовать цикл по всем 50 штатам, создавая вопрос для каждого из них
+	for questionNum in range(50):
+		# Получение правильных и неправильных ответов
+		correctAnswer = capitals[states[questionNum]]
+		wrongAnswers = list(capitals.values())
+		del wrongAnswers[wrongAnswers.index(correctAnswer)]
+		wrongAnswers = random.sample(wrongAnswers, 3)
+		answerOptions = wrongAnswers + [correctAnswer]
+		random.shuffle(answerOptions)
+		
+		# Записать варианты вопросов и ответов в файл билета
+		quizFile.write('%s Выберите столицу штата %s\n' % (questionNum + 1, states[questionNum]))
+		for i in range(4):
+			quizFile.write('    %s. %s\n' % ('ABCD'[i], answerOptions[i]))
+			quizFile.write('\n')
+		
+		# Записать ключ ответа в файл
+		answerKeyFile.write('%s. %s\n' % (questionNum + 1, 'ABCD'[answerOptions.index(correctAnswer)]))
+	quizFile.close()
+	answerKeyFile.close()
+	
---
+++ usingRegExp/phones_and_emailes/usingRegExp.py
@@ -0,0 +1,50 @@
+import pyperclip
+import re
+
+phoneRegex = re.compile(r'(\+7|8)?(\(\d{3}\))?(\d{3})(\s|-|\.)((\d{4})|((\d{2})(\s|-|\.)(\d{2})))')
+emailRegex = re.compile(r'([a-zA-Z0-9._%+-]+)(@)([a-zA-Z0-9.-]+)(\.[a-zA-Z]{2,4})')
+
+text = str(pyperclip.paste())
+
+catalog = {'phones': [],'emailes': []}
+phones, emailes = 0, 0
+
+with open('.\catalog\catalog.txt') as inf:
+	for line in inf:
+		line = line.strip()
+		odj = phoneRegex.search(line)
+		if odj != None:
+			catalog['phones'].append(odj.group())
+			continue
+		odj = emailRegex.search(line)
+		if odj != None:
+			catalog['emailes'].append(odj.group())
+
+for groups in phoneRegex.findall(text):
+	if ''.join([groups[0], groups[1], groups[2], '-', groups[4]]) in catalog['phones']:
+		continue
+	else:
+		catalog['phones'].append(''.join([groups[0], groups[1], groups[2], '-', groups[4]]))
+		phones += 1
+
+for groups in emailRegex.findall(text):
+	if ''.join(groups) in catalog['emailes']:
+		continue
+	else:
+		catalog['emailes'].append(''.join(groups))
+		emailes += 1
+
+with open('.\catalog\catalog.txt', 'w') as ouf:
+	ouf.write('PHONES:')
+	if phones == 0:
+		print('\nNo phone numbers found.\n')
+	for cell in catalog['phones']:
+		ouf.write('\n' + cell)
+		
+	
+	ouf.write('\n\nEMAIL ADDRESSES:')
+	if emailes == 0:
+		print('No email addresses found.')
+	for cell in catalog['emailes']:
+		ouf.write('\n' + cell)
+		
